98b5d088480f8105084dee68615ed1e9d22efd21
mahikumar <maheshkumawat0607@gmail.com>
2026-04-22 20:00:44 +0530

    update

--- src/lib/razorpay.ts
+++ src/lib/razorpay.ts
@@ -1,14 +1,6 @@
 import Razorpay from 'razorpay';
 
-if (!process.env.RAZORPAY_KEY_ID) {
-  throw new Error('RAZORPAY_KEY_ID is not defined');
-}
-
-if (!process.env.RAZORPAY_KEY_SECRET) {
-  throw new Error('RAZORPAY_KEY_SECRET is not defined');
-}
-
 export const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: process.env.RAZORPAY_KEY_ID || '',
+  key_secret: process.env.RAZORPAY_KEY_SECRET || '',
 });
